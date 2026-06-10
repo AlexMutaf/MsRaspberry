a854a41b89fe2e50896f07d85107428fbc61c4c2
Jumpler <aleksmutafchiev@gmail.com>
2026-06-10 19:43:03 +0300

    Implemented the Main Menu
    + Added the Flask. It:
    - moves around when you hover over it.
    - makes a sound when you hover over it.
    - takes you to another game scene.
    
    + Added Main Menu music.
    + Added a placeholder for Ms. Raspberry.

---
+++ ms-raspberry/scripts/main_menu.gd
@@ -0,0 +1,61 @@
+extends Node2D
+
+# Pressing the button
+func _on_tips_pressed() -> void:
+	return
+	get_tree().change_scene_to_file("");
+
+func _on_shop_pressed() -> void:
+	return
+	get_tree().change_scene_to_file("");
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+# Hovering over the button.
+func _on_tips_mouse_entered() -> void:
+	$"Control/Hover Button".play()
+
+func _on_shop_mouse_entered() -> void:
+	$"Control/Hover Button".play()
+
+func _on_exit_mouse_entered() -> void:
+	$"Control/Hover Button".play()
+
+# Start button
+
+@onready var flask = $Flask
+@onready var start_button = $"Control/Invisible Start Button"
+
+@export var speed: float = 5
+@export var radius: float = 50
+@export var return_speed: float = 8
+
+var is_hovered: bool = false
+var angle: float = 0
+var center_pos: Vector2
+
+func _ready() -> void:
+	center_pos = flask.position
+
+func _process(delta: float) -> void:
+	var target_pos: Vector2
+	if is_hovered:
+		angle += speed * delta
+		var offset = Vector2(cos(angle), sin(angle)) * radius
+		target_pos = center_pos + offset
+	else:
+		target_pos = center_pos
+		angle = lerp(angle, 0.00, return_speed * delta)
+	flask.position = flask.position.lerp(target_pos, return_speed * delta)
+
+func _on_invisible_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/loading_screen.tscn")
+
+func _on_invisible_start_button_mouse_entered() -> void:
+	$"Control/Hover Flask".play()
+	is_hovered = true
+
+func _on_invisible_start_button_mouse_exited() -> void:
+	is_hovered = false
--- ms-raspberry/scripts/spinning_background.gd
+++ ms-raspberry/scripts/spinning_background.gd
@@ -1,8 +1,8 @@
 extends Sprite2D
 
-@export var rotation_speed : float = -20.0 # Speed
+@export var rotation_speed: float = -20.0 # Speed
 
 # The following function runs automatically every frame of the game
 func _process(delta: float) -> void:	
-	# Multiplying by delta ensures it spins smoothly regardless of your PC's frame rate.
+	# Multiplying by delta ensures it spins smoothly (delta time)
 	rotation_degrees += rotation_speed * delta
