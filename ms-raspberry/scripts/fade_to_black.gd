30a26c170ae33c6ddbe7daa4db1744d189043c85
Duner4 <kaloyan.g.drosev@gmail.com>
2026-06-15 16:43:30 +0300

    commit 2
    -Addded questions scene
    -added timer on the loading screen

--- ms-raspberry/scripts/fade_to_black.gd
+++ ms-raspberry/scripts/fade_to_black.gd
@@ -9,4 +9,4 @@ func start_fade_sequence():
 	anim_player.play("fade_out")
 	await anim_player.animation_finished
 	await get_tree().create_timer(0.5).timeout
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/questions.tscn")
